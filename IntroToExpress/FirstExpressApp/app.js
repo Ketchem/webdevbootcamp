925daa68c1effca91eca5446468264dae455e3be
Brian K <>
2019-05-15 16:02:00 -0400

    Installed Expressa and added a new app

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -1,1 +1,22 @@
-console.log('express app.js');
+var express = require("express");
+var app = express();
+
+// "/" -> Hi There
+app.get("/", function(req, res){
+    res.send("Hi There!");
+});
+
+// "/bye" -> Goodbye
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+// "/dog" -> Meow!
+app.get("/dog", function(req, res){
+    res.send("Meow!");
+});
+
+// Tell Express to listen for requests
+app.listen('3000', 'localhost', function(){
+    console.log("Server has Started");
+});
