c77927e98450a6aab9a25d03f2e35427a60e5e35
Brian K <ketchem2@wisc.edu>
2019-05-29 14:32:07 -0400

    EJS conditionals and loops

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -5,9 +5,18 @@ app.get('/', function(req, res){
     res.render('home.ejs');
 });
 
-app.get('/fallinlove/:thing', function(req, res){
+app.get('/fallinlovewith/:thing', function(req, res){
     var thing = req.params.thing;
     res.render('love.ejs', {thingVar:thing});
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Smith"},
+        {title: "Pet Bunny", author: "Jones"},
+        {title: "Pet Dog", author: "Johnson"}
+    ]
+    res.render('posts.ejs', {posts: posts});
 });
 
 app.listen('3000','localhost', function(){
